80c91fb352d1c11ce0873fb55928ebe80e35d873
Nadun Lanka <ihnadun@gmail.com>
2023-11-25 19:45:48 +0530

    db changes

--- lib/db.ts
+++ lib/db.ts
@@ -1,10 +1,11 @@
 import { Sequelize } from 'sequelize-typescript';
 
 const sequelize = new Sequelize({
+  host: "ls-7735bb298beaa3e18908d34fc333915dc1ee6527.caeujlcqgxxe.ap-southeast-1.rds.amazonaws.com",
   database: 'ticket',
   dialect: 'mysql',
   username: 'root',
-  password: '',
+  password: 'Asd!!#123',
   models: [__dirname + '/models'],
 });
 
